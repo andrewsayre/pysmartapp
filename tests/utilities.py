3a0914d6d42466a8f8ea97c3f0faee0391a105f6
andrewsayre <6730289+andrewsayre@users.noreply.github.com>
2021-11-17 21:16:33 -0600

    Update dependencies

--- tests/utilities.py
+++ tests/utilities.py
@@ -4,8 +4,8 @@ import json
 
 def get_fixture(file: str, ext: str = 'json'):
     """Load a fixtures file."""
-    file_name = "tests/fixtures/{file}.{ext}".format(file=file, ext=ext)
-    with open(file_name) as open_file:
+    file_name = F"tests/fixtures/{file}.{ext}"
+    with open(file_name, encoding="utf-8") as open_file:
         if ext == 'json':
             return json.load(open_file)
         return open_file.read()
